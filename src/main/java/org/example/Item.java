c9b9c53b5dfebc8b202ef15cc99fc7749d5fb8d5
sshahd-ahmed <shahd.asaid@gmail.com>
2024-05-26 14:54:16 +0300

    adding item and item handler classes

---
+++ src/main/java/org/example/Item.java
@@ -0,0 +1,51 @@
+package org.example;
+public class Item {
+
+    public Item() {
+    }
+
+    /**
+     *
+     */
+    private int itemID;
+
+    /**
+     *
+     */
+    public String name;
+
+    /**
+     *
+     */
+    public String description;
+
+    /**
+     *
+     */
+    public double price;
+
+    /**
+     *
+     */
+    public double rating;
+
+    /**
+     *
+     */
+    public String review;
+
+    /**
+     *
+     */
+    public int categoryID;
+
+    /**
+     *
+     */
+    public int menuID;
+
+    /**
+     *
+     */
+    public String supplierID;
+}
---
+++ src/main/java/org/example/ItemHandler.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class ItemHandler {public ItemHandler() {
+}
+
+    /**
+     *
+     */
+
+    /**
+     * @return
+     */
+    public Item createItem() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param itemID
+     * @return
+     */
+    public Item getItem(int itemID) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param itemID
+     */
+    public void updateItem(int itemID) {
+        // TODO implement here
+    }
+
+    /**
+     * @param itemID
+     */
+    public void deleteItem(int itemID) {
+        // TODO implement here
+    }
+}
