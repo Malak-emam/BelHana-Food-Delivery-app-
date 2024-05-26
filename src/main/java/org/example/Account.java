97b810a5ebf5095356be9e11b6ac17e7e64b36d0
shadenelnaggar <shadensherif987@gmail.com>
2024-05-26 13:37:02 +0300

    Created Account Class

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.time.LocalDate;
+
+public class Account {
+    private int customerID;
+    private String accountNO;
+    private String typeOfAccount;
+    private int creditCardNo;
+    private LocalDate creditCardDate;
+
+    public Account() {
+    }
+}
