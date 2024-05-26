4b541ac0201389aeeec33ee556027888f4ee57ef
mayar1811 <mayar5872@gmail.com>
2024-05-26 15:18:14 +0300

    Adding Prodile Handler

---
+++ src/main/java/org/example/ProfileHandler.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class ProfileHandler {
+    public ProfileHandler() {
+    }
+
+    /**
+     * @param customerID
+     * @return
+     */
+    public Profile createProfile(int customerID) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param customerID
+     * @return
+     */
+    public Profile getProfile(int customerID) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param customerID
+     */
+    public void updateProfile(int customerID) {
+        // TODO implement here
+    }
+
+    /**
+     * @param customerID
+     */
+    public void deleteProfile(int customerID) {
+        // TODO implement here
+    }
+
+    /**
+     * @param customerID
+     * @return
+     */
+    public boolean authenticate(int customerID) {
+        // TODO implement here
+        return false;
+    }
+}
