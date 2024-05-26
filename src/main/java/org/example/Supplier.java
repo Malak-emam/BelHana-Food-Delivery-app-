ca21320660636d6907e730b5c924a56680a03274
Farida Abdelbaky Hassan <faridaabdelbakyhassan@Faridas-MacBook-Air-2.local>
2024-05-26 14:30:43 +0300

    adding supplier class

---
+++ src/main/java/org/example/Supplier.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Supplier {
+    /**
+     * Default constructor
+     */
+    public Supplier() {
+    }
+
+    /**
+     *
+     */
+    private int supplierID;
+
+    /**
+     *
+     */
+    public String supplierName;
+
+    /**
+     *
+     */
+    public int itemID;
+
+}
