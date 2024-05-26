4a1964f9785bbb123f50178002f5bbf78a3638a6
mayar1811 <mayar5872@gmail.com>
2024-05-26 13:22:38 +0300

    Adding Profile Class

---
+++ src/main/java/org/example/Profile.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import jdk.incubator.vector.VectorOperators;
+
+public class Profile {
+    /**
+     * Default constructor
+     */
+    public Profile() {
+    }
+
+    /**
+     *
+     */
+    private int customerID;
+
+    /**
+     *
+     */
+    private int username;
+
+    /**
+     *
+     */
+    public String dateOfBirth;
+
+    /**
+     *
+     */
+    private String address;
+
+    /**
+     *
+     */
+    private VectorOperators.Binary profilePhoto;
+
+    /**
+     * @param customerID
+     */
+    public void getProfileInfo(int customerID) {
+        // TODO implement here
+    }
+
+    /**
+     * @param customerID
+     */
+    public void deleteProfile(int customerID) {
+        // TODO implement here
+    }
+
+    /**
+     * @param customerID
+     */
+    public void updateUsername(int customerID) {
+        // TODO implement here
+    }
+
+    /**
+     * @param customerID
+     */
+    public void updatePhoneNo(int customerID) {
+        // TODO implement here
+    }
+
+    /**
+     * @param customerID
+     */
+    public void editProfilePhoto(int customerID) {
+        // TODO implement here
+    }
+
+    /**
+     * @param customerID
+     */
+    public void updateAddress(int customerID) {
+        // TODO implement here
+    }
+
+}
