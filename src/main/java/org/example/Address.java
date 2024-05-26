3b01879aee380b554e8fd465bddac9bc168cc38d
Malak-emam <emalk529@gmail.com>
2024-05-26 13:10:27 +0300

    Adding Address Class

---
+++ src/main/java/org/example/Address.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Address {
+
+    /**
+     * Default constructor
+     */
+    public Address() {
+    }
+
+    /**
+     *
+     */
+    private int customerID;
+
+    /**
+     *
+     */
+    private String street;
+
+    /**
+     *
+     */
+    private String apartmentNo;
+
+    /**
+     *
+     */
+    private String state;
+
+    /**
+     *
+     */
+    private String city;
+
+    /**
+     *
+     */
+    private String postalCode;
+
+}
