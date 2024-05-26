d09394a025d1adba6edc75c751211856fe81bd9d
Malak-emam <emalk529@gmail.com>
2024-05-26 15:27:20 +0300

    Adding category and categoryhandler classes

---
+++ src/main/java/org/example/Category.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Category {
+    public Category() {
+    }
+
+    /**
+     *
+     */
+    private int categoryID;
+
+    /**
+     *
+     */
+    public String name;
+
+    /**
+     *
+     */
+    public int menuID;
+
+    /**
+     * @param Menu menu
+     * @return
+     */
+    public boolean AddMenu(Menu menu) {
+        // TODO implement here
+        return false;
+    }
+
+    /**
+     * @param Menu menu
+     * @return
+     */
+    public boolean RemoveMenu(Menu menu) {
+        // TODO implement here
+        return false;
+    }
+}
---
+++ src/main/java/org/example/CategoryHandler.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.awt.*;
+import java.util.List;
+
+public class CategoryHandler {
+    public CategoryHandler() {
+    }
+
+    /**
+     * @return
+     */
+    public Category createCategory() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param categoryID
+     * @param itemID
+     */
+    public void addItemToCategory(int categoryID, int itemID) {
+        // TODO implement here
+    }
+
+    /**
+     * @param categoryD
+     * @return
+     */
+    public List<Item> getCategory(int categoryD) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param categoryID
+     */
+    public void updateCategory(int categoryID) {
+        // TODO implement here
+    }
+
+    /**
+     * @param categoryID
+     */
+    public void deleteCategory(int categoryID) {
+        // TODO implement here
+    }
+}
