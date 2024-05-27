489155ca7144681537c6c370d4429459f6d64e81
Malak-emam <emalk529@gmail.com>
2024-05-27 19:15:10 +0300

    adding Salad, Soup, and Veggies classes

---
+++ src/main/java/org/example/Salad.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Salad {
+    public Salad() {
+    }
+
+    /**
+     *
+     */
+    private String SAName;
+
+    /**
+     *
+     */
+    public void Salad() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getSaladName() {
+        // TODO implement here
+        return "";
+    }
+}
---
+++ src/main/java/org/example/Soup.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Soup {
+    public Soup() {
+    }
+
+    /**
+     *
+     */
+    private String SOName;
+
+    /**
+     *
+     */
+    public void Soup() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getSoupName() {
+        // TODO implement here
+        return "";
+    }
+}
