dd1950aaa3aae09f432cd4d176ece191719a7090
Farida Abdelbaky Hassan <faridaabdelbakyhassan@Faridas-MacBook-Air-2.local>
2024-05-27 15:52:43 +0300

    Added ObserverAdmin and CustomerAdmin, potential del

---
+++ src/main/java/org/example/CustomerAdmin.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class CustomerAdmin {
+    public CustomerAdmin() {
+    }
+
+    /**
+     * @param status
+     */
+    public void update(String status) {
+        // TODO implement here
+    }
+}
---
+++ src/main/java/org/example/ObserverAdmin.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ObserverAdmin {
+    public ObserverAdmin() {
+    }
+
+    /**
+     * @param status
+     */
+    public void update(String status) {
+        // TODO implement here
+    }
+}
