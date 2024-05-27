4b432be0730b5d784367ac4161c4225a70edeba7
Malak-emam <emalk529@gmail.com>
2024-05-27 18:35:09 +0300

    adding database admin class

---
+++ src/main/java/org/example/DatabaseAdmin.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class DatabaseAdmin {
+    public DatabaseAdmin() {
+    }
+
+    /**
+     *
+     */
+    private DatabaseAdmin instance;
+
+    /**
+     *
+     */
+    private void DatabaseAdmin() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public DatabaseAdmin GetInstance() {
+        // TODO implement here
+        return null;
+    }
+}
